6a9fd3504213b0727b538c9c6a41ffdcfa0ce263
Carotus <120000954+Carotus@users.noreply.github.com>
2023-04-26 16:13:40 +0200

    fix butterfly

--- Assets/Scripts/Player/Farfallina.cs
+++ Assets/Scripts/Player/Farfallina.cs
@@ -9,9 +9,10 @@ public class Farfallina : MonoBehaviour
 
     private void Update()
     {
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Mathf.Abs(Camera.main.transform.position.z);
+        Vector3 difference = Camera.main.ScreenToWorldPoint(mousePos) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
-
     }
 }
